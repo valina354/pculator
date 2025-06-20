d686052ad466b0bdfc03d88372843237cd7d0939
Mike Chambers <>
2025-06-19 22:22:15 -0500

    Accidentally removed jart's licensing notice from the top of the FPU module. Added it back in.

--- PCulator/cpu/fpu.c
+++ PCulator/cpu/fpu.c
@@ -1,4 +1,28 @@
-﻿#include "fpu.h"
+﻿/*-*- mode:c;indent-tabs-mode:nil;c-basic-offset:2;tab-width:8;coding:utf-8 -*-│
+│ vi: set et ft=c ts=2 sts=2 sw=2 fenc=utf-8                               :vi │
+╞══════════════════════════════════════════════════════════════════════════════╡
+│ Copyright 2022 Justine Alexandra Roberts Tunney                              │
+│                                                                              │
+│ Permission to use, copy, modify, and/or distribute this software for         │
+│ any purpose with or without fee is hereby granted, provided that the         │
+│ above copyright notice and this permission notice appear in all copies.      │
+│                                                                              │
+│ THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL                │
+│ WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED                │
+│ WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE             │
+│ AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL         │
+│ DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR        │
+│ PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER               │
+│ TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR             │
+│ PERFORMANCE OF THIS SOFTWARE.                                                │
+╚─────────────────────────────────────────────────────────────────────────────*/
+
+// This FPU code was adapted for PCulator from the Blink emulator by Justine Tunney (aka jart)
+// I want to write my own from scratch at some point, but this works well for now.
+//
+// Blink source code: https://github.com/jart/blink/
+
+#include "fpu.h"
 
 #include <math.h>
 #include <stdio.h>
@@ -156,7 +180,7 @@ double DeserializeLdbl(const u8 b[10]) {
 static i16 FpuGetMemoryShort(struct Machine* m) {
     //u8 b[2];
     //return Read16(Load(m, m->fpu.dp, 2, b));
-    return Read16(m->fpu.dp); //TODO: Is this right? This guy's code is full of confusing, unnecessary abstractions...
+    return Read16(m->fpu.dp);
 }
 
 static void FpuSetMemoryShort(struct Machine* m, i16 i) {
@@ -164,7 +188,7 @@ static void FpuSetMemoryShort(struct Machine* m, i16 i) {
     //u8 b[2];
     //Write16(BeginStore(m, m->fpu.dp, 2, p, b), i);
     //EndStore(m, m->fpu.dp, 2, p, b);
-    Write16(m->fpu.dp, i); //TODO?
+    Write16(m->fpu.dp, i);
 }
 
 static void OpFstcw(struct Machine* m) {
